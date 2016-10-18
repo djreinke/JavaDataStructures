ed87d1403f081aae8cc434008ac7a483384f6651
Daniel Reinke <djreinke@olivet.edu>
2016-10-17 20:28:05 -0500

    Progress on Binary Search Tree

--- src/dataStructures/BinarySearchTree.java
+++ src/dataStructures/BinarySearchTree.java
@@ -8,5 +8,48 @@ package dataStructures;
  *
  */
 public class BinarySearchTree {
+	
 	private BinarySearchTreeNode root;
+	
+	public BinarySearchTree(){
+		this.root = null;
+	}
+	
+	public BinarySearchTree(BinarySearchTreeNode n){
+		this.root = n;
+	}
+
+	public boolean isEmpty(){
+		return this.root == null;
+	}
+	
+	public void insert(BinarySearchTreeNode node){
+		this.root = insert(this.root, node);
+	}
+	
+	private BinarySearchTreeNode insert( BinarySearchTreeNode p, BinarySearchTreeNode current ){
+		if (p.isEmpty())
+			return new BinarySearchTreeNode(current.getData());
+		if (current.getData() == p.getData())
+			return p;
+		if (current.getData() < p.getData())
+			p.left = insert(p.getLeftNode(), current);
+		else
+			p.right = insert(p.getRightNode(), current);
+		return p;
+	}
+	
+	public void display(BinarySearchTreeNode root){
+		if(root != null){
+			display(root.getLeftNode());
+			System.out.print(" " + root.getData());
+			display(root.getRightNode());
+		}
+	}
+	
+	public static void main(String args[]){
+		BinarySearchTree bst = new BinarySearchTree();
+		bst.insert(new BinarySearchTreeNode(10));
+		bst.display(bst.root);
+	}
 }
--- src/dataStructures/BinarySearchTreeNode.java
+++ src/dataStructures/BinarySearchTreeNode.java
@@ -8,5 +8,47 @@ package dataStructures;
  *
  */
 public class BinarySearchTreeNode {
-
+	
+	BinarySearchTreeNode left, right;
+	int nodeData;
+	
+	public BinarySearchTreeNode(){
+		this.left = null;
+		this.right = null;
+		this.nodeData = -1;
+	}
+	
+	public BinarySearchTreeNode(int nodeData){
+		this.left = null; 
+		this.right = null;
+		this.nodeData = nodeData;
+	}
+	
+	public void SetLeftNode(BinarySearchTreeNode n){
+		this.left = n;
+	}
+	
+	public void SetRightNode(BinarySearchTreeNode n){
+		this.right = n;
+	}
+	
+	public BinarySearchTreeNode getLeftNode(){
+		return this.left;
+	}
+	
+	public BinarySearchTreeNode getRightNode(){
+		return this.right;
+	}
+	
+	public void setData(int nodeData){
+		this.nodeData = nodeData;
+	}
+	
+	public int getData(){
+		return this.nodeData;
+	}
+	
+	public boolean isEmpty(){
+		return this.nodeData == -1;
+	}
 }
